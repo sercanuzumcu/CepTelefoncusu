c66e1abb51944c97a19417f811669801fd061a95
sercanuzumcu <sercanuzumcu@gmail.com>
2014-09-11 20:34:56 +0300

    İlk Giriş

---
+++ CepTelefoncusu/Backup/CepTelefoncusu/Brands/List.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="List.aspx.cs" Inherits="CepTelefoncusu.Brands.List" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Cep Telefoncu Hilmi</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <ul>
+        <asp:Repeater ID="rptBrand" runat="server">
+        <ItemTemplate>
+            <li> <%#Eval("BrandText") %></li>
+        </ItemTemplate>
+        </asp:Repeater>
+        <li><a href="/Brands/Add.aspx">Marka Ekle</a></li>
+    </ul>
+    </div>
+    </form>
+</body>
+</html>
---
+++ CepTelefoncusu/Backup/CepTelefoncusu/Classes/Brand.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace CepTelefoncusu.Classes
+{
+    public class Brand : DbConn
+    {
+        public int Id { get; set; }
+        public String BrandText { get; set; }
+
+        // Insert
+        public int Insert() {
+            int phoneId = 0;
+            
+            String sql = "insert into Brands (BrandText) VALUES (@BrandText)";
+            
+            SqlCommand cmd = new SqlCommand(sql, cnn);
+            cmd.Parameters.AddWithValue("@BrandText", this.BrandText);
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+            
+            // TODO: Return Last inserted Id
+
+            return phoneId;
+        
+        }
+        // Select
+
+        public List<Brand> GetAll() 
+        {
+            List<Brand> brands = new List<Brand>();
+
+            String sql = "select * from Brands";
+            SqlCommand cmd = new SqlCommand(sql, cnn);
+            cnn.Open();
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read()) 
+            {
+                //int id = reader.GetInt32(0);
+                //String brandText = reader.["BrandText"].ToString();
+
+                Brand b = new Brand();
+                b.Id = reader.GetInt32(0);
+                b.BrandText = reader["BrandText"].ToString();
+
+                brands.Add(b);
+
+                
+            }
+            cnn.Close();
+            return brands;
+        }
+
+        // Select All
+
+        // Select One
+
+        // Update
+    }
+}
